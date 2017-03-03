170657aae79516bc380aa21a2252315d04f5c290
benofben <ben.lackey@outlook.com>
2017-03-03 00:33:46 -0800

    url for new org

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -4,8 +4,8 @@ RESOURCE_GROUP=$1
 
 # CLI 1.0 commands
 #azure group create $1
-#azure group deployment create --template-uri http://raw.githubusercontent.com/benofben/couchbase-azure-resource-manager/master/simple/mainTemplate.json --parameters-file mainTemplateParameters.json $RESOURCE_GROUP couchbase
+#azure group deployment create --template-uri https://raw.githubusercontent.com/couchbase-partners/azure-resource-manager-couchbase/master/simple/mainTemplate.json --parameters-file mainTemplateParameters.json $RESOURCE_GROUP couchbase
 
 # CLI 2.0 commands
 az group create --name $RESOURCE_GROUP --location westus
-az group deployment create --template-uri http://raw.githubusercontent.com/benofben/couchbase-azure-resource-manager/master/simple/mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP
+az group deployment create --template-uri https://raw.githubusercontent.com/couchbase-partners/azure-resource-manager-couchbase/master/simple/mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP
