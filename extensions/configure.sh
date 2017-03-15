86da23e92bfcefa05bfb23ef86efe29367b4261f
benofben <ben.lackey@outlook.com>
2017-03-15 14:03:41 -0700

    separate scripts
    Former-commit-id: d2f01a66d0f3a8a645cf2912d11e5fe7ffc363e6

---
+++ extensions/configure.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+echo "Running configure.sh"
+
+adminUsername=$1
+adminPassword=$2
+nodeCount=$3
+
+echo "Using the settings:"
+echo adminUsername \'$adminUsername\'
+echo adminPassword \'$adminPassword\'
+echo nodeCount \'$nodeCount\'
+
+# Using these instructions
+# https://developer.couchbase.com/documentation/server/4.6/install/init-setup.html
+cd /opt/couchbase/bin/
+
+totalRAM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+dataRAM=$((60 * $totalRAM / 100000))
+indexRAM=$((20 * $totalRAM / 100000))
+
+./couchbase-cli cluster-init \
+--cluster-ramsize=$dataRAM \
+--cluster-index-ramsize=$indexRAM \
+--cluster-username=$adminUsername \
+--cluster-password=$adminPassword
+
+for (( i=1; i<$nodeCount; i++ ))
+do
+  echo "Adding node vm$i to the cluster."
+  nodePrivateDNS=`host vm$i | awk '{print $1}'`
+  ./couchbase-cli server-add \
+  --cluster=vm0 \
+  --user=$adminUsername \
+  --pass=$adminPassword \
+  --server-add=$nodePrivateDNS \
+  --server-add-username=$adminUsername \
+  --server-add-password=$adminPassword
+done
+
+./couchbase-cli rebalance \
+--cluster=vm0 \
+--user=$adminUsername \
+--pass=$adminPassword
