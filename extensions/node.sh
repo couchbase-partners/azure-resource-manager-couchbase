1bcdbfe908e970a89a70b1b2d922a4f48125794f
benofben <ben.lackey@outlook.com>
2017-03-14 22:29:23 -0700

    going to try cli instead.
    Former-commit-id: 60c10e77316b049947dc5d86be80a22ef7a28597

--- extensions/node.sh
+++ extensions/node.sh
@@ -15,6 +15,10 @@ echo nodeIndex \'$nodeIndex\'
 echo uniqueString \'$uniqueString\'
 echo location \'$location\'
 
+# This is the rally point for our cluster
+# an example DNS record is vm0-ixymcna6yezhc.westus.cloudapp.azure.com
+rallyPointDNS="vm0-"$uniqueString"."$location".cloudapp.azure.com"
+
 ##### Install Couchbase
 
 # we're currently caching the binary in GitHub.  This is not a good solution.
@@ -34,7 +38,26 @@ apt-get -y install couchbase-server
 # Using these instructions
 # https://developer.couchbase.com/documentation/server/4.6/install/init-setup.html
 
-# curl -v -X POST -u Administrator:asdasd http://127.0.0.1:8091/node/controller/rename -d hostname=shz.localdomain
-
 # if we're the first node then we're going to create a new cluster, otherwise
 # we'll just join the existing one.
+
+# Set up services
+
+# the doc has an example with data and query as types. I'm getting unknown for
+# those, so they may be deprecated.
+
+# seem like options are: index, fts, n1ql, kv
+# I'm getting a memory allocation issue when I try to do all four.  Trying without fts for now.
+curl -v http://localhost:8091/node/controller/setupServices -d 'services=kv%2Cn1ql%2Cindex'
+
+# Initialize a node
+curl -v http://localhost:8091/nodes/self/controller/settings -d 'path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata&index_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata'
+
+# Set up your administrator-username and password
+# doesn't seem to work
+# Error is: All parameters must be given
+curl -v http://localhost:8091/settings/web -d password=$adminPassword -d username=$adminUsername
+
+curl -v http://localhost:8091/controller/addNode -d hostname=$rallyPointDNS -d 'services=kv%2Cn1ql%2Cindex'
+
+curl -v http://localhost:8091/controller/rebalance
