c39d1d266f4d5cf0b042dd3ec95104d0de7c392b
benofben <ben.lackey@outlook.com>
2017-03-14 18:50:47 -0700

    pass dns

--- extensions/node.sh
+++ extensions/node.sh
@@ -2,6 +2,14 @@
 
 echo "Running node.sh"
 
+adminUsername=$1
+adminPassword=$2
+nodeDNS=$3
+
 echo "Configuring nodes with the settings:"
 echo adminUsername \'$adminUsername\'
 echo adminPassword \'$adminPassword\'
+echo nodeDNS \'$nodeDNS\'
+
+# Using these instructions
+# https://developer.couchbase.com/documentation/server/4.6/install/install-linux.html
