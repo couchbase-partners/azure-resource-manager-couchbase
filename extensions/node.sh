2db54a4a038b3eb18ee88ac371b1803887985964
benofben <ben.lackey@outlook.com>
2017-03-14 20:33:54 -0700

    adding things
    Former-commit-id: 1f0e7b0405d332df948ddcea1e9609e4db57487a

--- extensions/node.sh
+++ extensions/node.sh
@@ -14,10 +14,18 @@ echo nodeDNS \'$nodeDNS\'
 # There are some post config steps including paging, NIC settings, etc that we should add
 # https://developer.couchbase.com/documentation/server/4.6/install/install-linux.html
 
-# Using these instructions
-# https://developer.couchbase.com/documentation/server/4.6/install/ubuntu-debian-install.html
+##### Install Couchbase
+
 # we're currently caching the binary in GitHub.  This is not a good solution.
 wget https://github.com/couchbase-partners/azure-resource-manager-couchbase/raw/master/extensions/couchbase-server-enterprise_4.6.1-debian8_amd64.deb
+
+# Using these instructions
+# https://developer.couchbase.com/documentation/server/4.6/install/ubuntu-debian-install.html
 dpkg -i couchbase-server-enterprise_4.6.1-debian8_amd64.deb
 apt-get update
 apt-get install couchbase-server
+
+##### Configure Couchbase
+
+# Using these instructions
+# https://developer.couchbase.com/documentation/server/4.6/install/init-setup.html
