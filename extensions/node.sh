54fefee1d12bfd6a34ee458b910244335c5c7b86
benofben <ben.lackey@outlook.com>
2017-03-15 00:13:45 -0700

    more comment about default config
    Former-commit-id: ee41e48c828a414792a60da176fe982d5ae2e292

--- extensions/node.sh
+++ extensions/node.sh
@@ -29,6 +29,12 @@ apt-get -y install couchbase-server
 # There are some post config steps including paging, NIC settings, etc that we should add
 # https://developer.couchbase.com/documentation/server/4.6/install/install-linux.html
 
+#Warning: Transparent hugepages looks to be active and should not be.
+#Please look at http://bit.ly/1ZAcLjD as for how to PERMANENTLY alter this setting.
+
+#Warning: Swappiness is not set to 0.
+#Please look at http://bit.ly/1k2CtNn as for how to PERMANENTLY alter this setting.
+
 ###############################
 ##### Configure Couchbase #####
 ###############################
