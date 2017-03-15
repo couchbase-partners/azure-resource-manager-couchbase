730031222d253c8914e51a5bd7f17793ed41ace6
benofben <ben.lackey@outlook.com>
2017-03-14 19:18:09 -0700

    starting over on the node script

--- extensions/node.sh
+++ extensions/node.sh
@@ -11,5 +11,10 @@ echo adminUsername \'$adminUsername\'
 echo adminPassword \'$adminPassword\'
 echo nodeDNS \'$nodeDNS\'
 
-# Using these instructions
+# There are some post config steps including paging, NIC settings, etc that we should add
 # https://developer.couchbase.com/documentation/server/4.6/install/install-linux.html
+
+# Using these instructions
+# https://developer.couchbase.com/documentation/server/4.6/install/ubuntu-debian-install.html
+
+#curl -O http://packages.couchbase.com/releases/couchbase-release/couchbase-server-enterprise_4.6-1-debian8_amd64.deb
