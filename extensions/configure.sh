7c8347ec618c1505497ace40308c5fdaa5f53ac3
benofben <ben.lackey@outlook.com>
2017-03-16 09:54:23 -0700

    single extension refactor.  needs retry if this works
    Former-commit-id: 93910b1c37ba6fa880bbe7140bbbe6a299f84386

--- extensions/configure.sh
+++ extensions/configure.sh
@@ -4,31 +4,34 @@ echo "Running configure.sh"
 
 adminUsername=$1
 adminPassword=$2
-nodeCount=$3
+nodeIndex=$3
+nodeCount=$4
 
 echo "Using the settings:"
 echo adminUsername \'$adminUsername\'
 echo adminPassword \'$adminPassword\'
+echo nodeIndex \'$nodeIndex\'
 echo nodeCount \'$nodeCount\'
 
 # Using these instructions
 # https://developer.couchbase.com/documentation/server/4.6/install/init-setup.html
 cd /opt/couchbase/bin/
 
-totalRAM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-dataRAM=$((60 * $totalRAM / 100000))
-indexRAM=$((20 * $totalRAM / 100000))
+if [[ $nodeIndex == "0" ]]
+then
+  totalRAM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+  dataRAM=$((60 * $totalRAM / 100000))
+  indexRAM=$((20 * $totalRAM / 100000))
 
-./couchbase-cli cluster-init \
---cluster-ramsize=$dataRAM \
---cluster-index-ramsize=$indexRAM \
---cluster-username=$adminUsername \
---cluster-password=$adminPassword
+  ./couchbase-cli cluster-init \
+  --cluster-ramsize=$dataRAM \
+  --cluster-index-ramsize=$indexRAM \
+  --cluster-username=$adminUsername \
+  --cluster-password=$adminPassword
+else
+  echo "Adding node vm$nodeIndex to the cluster."
+  nodePrivateDNS=`host vm$nodeIndex | awk '{print $1}'`
 
-for (( i=1; i<$nodeCount; i++ ))
-do
-  echo "Adding node vm$i to the cluster."
-  nodePrivateDNS=`host vm$i | awk '{print $1}'`
   ./couchbase-cli server-add \
   --cluster=vm0 \
   --user=$adminUsername \
@@ -36,8 +39,10 @@ do
   --server-add=$nodePrivateDNS \
   --server-add-username=$adminUsername \
   --server-add-password=$adminPassword
-done
+fi
 
+# Ideally we want to test if the cluster has added all the nodes and then call rebalance.
+# For now we're just going to call it every time we run this script
 ./couchbase-cli rebalance \
 --cluster=vm0 \
 --user=$adminUsername \
