a163034e7fd9d1ebc331e90412be7f7be49fe578
benofben <ben.lackey@outlook.com>
2017-03-21 15:26:11 -0700

    cut trent's stuff down to a few lines
    Former-commit-id: 0f5ab1ae6c61634079a393979ec8a3c8dcd41568

--- extensions/format.sh
+++ extensions/format.sh
@@ -4,3 +4,27 @@
 # sda - OS Disk
 # sdb - Ephemeral
 # sdc - Attached Disk
+
+echo "Partitioning the disk."
+DISK="/dev/sdc"
+echo "n
+p
+1
+
+
+t
+83
+w"| fdisk ${DISK}
+
+echo "Creating the filesystem."
+mkfs -j -t ext4 /dev/sdc1
+
+echo "Updating fstab"
+MOUNTPOINT="/datadisks/disk1"
+read UUID FS_TYPE < <(blkid -u filesystem ${PARTITION}|awk -F "[= ]" '{print $3" "$5}'|tr -d "\"")
+LINE="UUID=\"${UUID}\"\t${MOUNTPOINT}\text4\tnoatime,nodiratime,nodev,noexec,nosuid\t1 2"
+echo -e "${LINE}" >> /etc/fstab
+
+echo "Mounting the disk"
+mkdir -p ${MOUNTPOINT}
+mount ${MOUNTPOINT}
