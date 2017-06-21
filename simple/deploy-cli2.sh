bc871ff0c5c2ce8e0a98d1d797fedfb92ac2e508
benofben <ben.lackey@outlook.com>
2017-06-20 17:41:57 -0700

    revert cli

--- simple/deploy-cli2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-RESOURCE_GROUP=$1
-
-# CLI 2.0 commands
-az group create --name $RESOURCE_GROUP --location westus
-az group deployment create --template-uri https://raw.githubusercontent.com/couchbase-partners/azure-resource-manager-couchbase/master/simple/mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP
