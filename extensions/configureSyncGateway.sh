58c936bcfed0284dc47c3c5778be5a8f6275f2b7
benofben <ben.lackey@outlook.com>
2017-08-15 21:07:52 -0700

    drop unused username

--- extensions/configureSyncGateway.sh
+++ extensions/configureSyncGateway.sh
@@ -2,14 +2,10 @@
 
 echo "Running configureSyncGateway.sh"
 
-adminUsername=$1
-adminPassword=$2
-uniqueString=$3
-location=$4
+uniqueString=$1
+location=$2
 
 echo "Using the settings:"
-echo adminUsername \'$adminUsername\'
-echo adminPassword \'$adminPassword\'
 echo uniqueString \'$uniqueString\'
 echo location \'$location\'
 
--- extensions/syncGateway.sh
+++ extensions/syncGateway.sh
@@ -2,17 +2,13 @@
 
 echo "Running syncGateway.sh"
 
-adminUsername=$1
-adminPassword=$2
-uniqueString=$3
-location=$4
+uniqueString=$1
+location=$2
 
 echo "Using the settings:"
-echo adminUsername \'$adminUsername\'
-echo adminPassword \'$adminPassword\'
 echo uniqueString \'$uniqueString\'
 echo location \'$location\'
 
 ./adjust_tcp_keepalive.sh
 ./installSyncGateway.sh
-./configureSyncGateway.sh $adminUsername $adminPassword $uniqueString $location
+./configureSyncGateway.sh $uniqueString $location
