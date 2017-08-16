4bcaa2f036de18a79dd9959a488ef124a5e43833
benofben <ben.lackey@outlook.com>
2017-08-15 21:04:56 -0700

    try loading the bucket

--- extensions/configureServer.sh
+++ extensions/configureServer.sh
@@ -62,6 +62,16 @@ then
     --bucket=default \
     --bucket-type=couchbase \
     --bucket-ramsize=100
+
+  echo "Loading the travel-sample"
+  ./cbdocloader \
+    -n $nodePublicDNS:8091 \
+    -u $adminUsername \
+    -p $adminPassword \
+    -b mybucket \
+    -s 100 \
+    /opt/couchbase/samples/travel-sample.zip
+
 else
   echo "Running couchbase-cli server-add"
   output=""
