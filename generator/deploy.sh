f41b7ef9f971ffc27a482beb3508772fcfb1c1fe
benofben <ben.lackey@outlook.com>
2017-08-28 16:37:36 -0700

    move to 2.0 commands

--- generator/deploy.sh
+++ generator/deploy.sh
@@ -6,6 +6,9 @@ RESOURCE_GROUP=$2
 # create generatedTemplate.json
 python deployment.py parameters/${PARAMETERS_FILE}.yaml
 
-# Azure CLI 1.0 commands
-#azure group create $RESOURCE_GROUP westus
-#azure group deployment create --template-file generatedTemplate.json $RESOURCE_GROUP couchbase
+az group create --name $RESOURCE_GROUP --location westus
+
+az group deployment create \
+--template-file @generatedTemplate.json \
+--parameters @mainTemplateParameters.json \
+--resource-group $RESOURCE_GROUP
--- simple/deploy.sh
+++ simple/deploy.sh
@@ -2,10 +2,9 @@
 
 RESOURCE_GROUP=$1
 
-# Azure CLI 1.0 commands
-#azure group create $RESOURCE_GROUP westus
-#azure group deployment create --template-uri https://raw.githubusercontent.com/couchbase-partners/azure-resource-manager-couchbase/master/simple/mainTemplate.json --parameters-file mainTemplateParameters.json $RESOURCE_GROUP couchbase
-
-# Azure CLI 2.0 commands
 az group create --name $RESOURCE_GROUP --location westus
-az group deployment create --template-uri https://raw.githubusercontent.com/couchbase-partners/azure-resource-manager-couchbase/master/simple/mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP
+
+az group deployment create \
+--template-file @mainTemplate.json \
+--parameters @mainTemplateParameters.json \
+--resource-group $RESOURCE_GROUP
