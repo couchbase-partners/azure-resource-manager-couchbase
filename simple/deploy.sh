783cad11be02a4c4938d71e1c0cdc40d8a59f1f8
benofben <ben.lackey@outlook.com>
2017-08-31 09:41:19 -0700

    fiddling w cli 2.0 commands per jason

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -6,6 +6,6 @@ RESOURCE_GROUP=$1
 azure group create $RESOURCE_GROUP westus
 azure group deployment create --template-file mainTemplate.json --parameters-file mainTemplateParameters.json $RESOURCE_GROUP
 
-# CLI 2.0 commands -- these don't print much info, so we're not using them.
+# CLI 2.0 commands
 #az group create --name $RESOURCE_GROUP --location westus --output json
-#az group deployment create --template-file mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP --output json
+#az group deployment create --verbose --template-file mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP --output json
