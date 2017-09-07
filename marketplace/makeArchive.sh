519ffc67de4c7c64f828520f167cdfe89c17c8a9
benofben <ben.lackey@outlook.com>
2017-09-06 21:08:16 -0700

    commit before running mk archive

--- marketplace/makeArchive.sh
+++ marketplace/makeArchive.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
-# This script creates a zip of our archive to publish in the marketplace
+# This script creates zips of our template to publish in the marketplace
 
-mkdir tmp
-cd tmp
+function makeArchive()
+{
+  license=$1
+  
+  mkdir tmp
+  cd tmp
 
-cp ../../extensions/* ./
-cp ../../simple/* ./
+  cp ../../extensions/* ./
+  cp ../../simple/* ./
 
-# Do this after and overwrite mainTemplate.json
-cp ../mainTemplate.json ./
-cp ../createUiDefinition.json ./
+  # Do this after and overwrite mainTemplate.json
+  cp ../mainTemplate-${license}.json ./mainTemplate.json
+  cp ../createUiDefinition.json ./
 
-# Drop some files that don't need to be in the archive
-rm README.md
-rm deploy.sh
-rm deploy.ps1
-rm mainTemplateParameters.json
+  # Drop some files that don't need to be in the archive
+  rm README.md
+  rm deploy.sh
+  rm deploy.ps1
+  rm mainTemplateParameters.json
 
-zip ../archive.zip *
-cd -
-rm -rf tmp
+  zip ../archive-${license}.zip *
+  cd -
+  rm -rf tmp
+}
+
+makeArchive byol
+markArchive hourly_pricing
