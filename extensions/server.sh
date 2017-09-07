53cce442777a3821ac1ba28edb514811e533e1a2
benofben <ben.lackey@outlook.com>
2017-09-06 20:38:48 -0700

    rev to 4.6.3

--- extensions/server.sh
+++ extensions/server.sh
@@ -14,8 +14,8 @@ echo uniqueString \'$uniqueString\'
 echo location \'$location\'
 
 echo "Installing Couchbase Server..."
-wget http://packages.couchbase.com/releases/4.6.2/couchbase-server-enterprise_4.6.2-ubuntu14.04_amd64.deb
-dpkg -i couchbase-server-enterprise_4.6.2-ubuntu14.04_amd64.deb
+wget http://packages.couchbase.com/releases/4.6.3/couchbase-server-enterprise_4.6.3-ubuntu14.04_amd64.deb
+dpkg -i couchbase-server-enterprise_4.6.3-ubuntu14.04_amd64.deb
 apt-get update
 apt-get -y install couchbase-server
 
