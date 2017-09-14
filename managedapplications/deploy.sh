deefea61dfc26f46eefc36dcf6d610a9be7f8bd4
benofben <ben.lackey@outlook.com>
2017-09-14 16:53:15 -0700

    working on deploy scipt

---
+++ managedapplications/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+RESOURCE_GROUP=$1
+
+az group create --name $RESOURCE_GROUP --location westcentralus
+az managedapp definition create \
+  -n couchbasedefinition \
+  -l westcentralus \
+  --resource-group $RESOURCE_GROUP \
+  --lock-level readOnly \
+  --display-name couchbasedescription \
+  --description "Some description here..." \
+  --authorizations "6588f057-f12f-43aa-9546-d545b00a6261:8e3af657-a8ff-443c-a75c-2fe8c4bcb635" \
+  --package-file-uri "https://managedapp.blob.core.windows.net/app1/archive.zip"
