67ca1f7e06e43aa5d70c8b6fee17bbc4385c9c9e
benofben <ben.lackey@outlook.com>
2017-09-14 17:11:08 -0700

    location and rename deploy

--- managedapplications/deployDefinition.sh
+++ managedapplications/deployDefinition.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RESOURCE_GROUP=$1
+RESOURCE_GROUP=managedAppDefinition
 
 az group create --name $RESOURCE_GROUP --location westcentralus
 az managedapp definition create \
@@ -8,8 +8,8 @@ az managedapp definition create \
   -l westcentralus \
   --resource-group $RESOURCE_GROUP \
   --lock-level readOnly \
-  --display-name couchbasedescription \
-  --description "Some description here..." \
+  --display-name Couchbase \
+  --description "Couchbase is the engagement database." \
   --authorizations "6588f057-f12f-43aa-9546-d545b00a6261:8e3af657-a8ff-443c-a75c-2fe8c4bcb635" \
   --package-file-uri "https://managedapp.blob.core.windows.net/app1/archive.zip" \
   --debug \
