4bf02493932f42c9a035e6b46431ffac1161678d
benofben <ben.lackey@outlook.com>
2017-09-19 22:00:43 -0700

    drop json output

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -7,5 +7,5 @@ azure group create $RESOURCE_GROUP westus
 azure group deployment create --template-file mainTemplate.json --parameters-file mainTemplateParameters.json $RESOURCE_GROUP
 
 # CLI 2.0 commands
-#az group create --name $RESOURCE_GROUP --location westus --output json
-#az group deployment create --verbose --template-file mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP --output json
+#az group create --name $RESOURCE_GROUP --location westus
+#az group deployment create --verbose --template-file mainTemplate.json --parameters @mainTemplateParameters.json --resource-group $RESOURCE_GROUP
