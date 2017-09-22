83239d2a2a1cb73844df91cca62ee49c430c2ac4
benofben <ben.lackey@outlook.com>
2017-09-22 13:36:37 -0700

    test drive update

--- testdrive/makeArchive.sh
+++ testdrive/makeArchive.sh
@@ -4,11 +4,6 @@ rm archive.zip
 mkdir tmp
 
 cp main-template.json tmp
-
-cp ../simple/networkSecurityGroups.json tmp
-cp ../simple/server.json tmp
-cp ../simple/syncGateway.json tmp
-
 cp ../extensions/* tmp
 
 zip -r -X archive.zip tmp
