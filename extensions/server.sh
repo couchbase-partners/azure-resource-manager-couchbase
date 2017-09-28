cfad0bed96cd5db396b4e4d7d6d9b0eddbd25145
benofben <ben.lackey@outlook.com>
2017-09-28 13:53:07 -0700

    add 5.0.0 dependency

--- extensions/server.sh
+++ extensions/server.sh
@@ -19,6 +19,7 @@ echo "Installing Couchbase Server..."
 wget http://packages.couchbase.com/releases/${version}/couchbase-server-enterprise_${version}-ubuntu14.04_amd64.deb
 dpkg -i couchbase-server-enterprise_${version}-ubuntu14.04_amd64.deb
 apt-get update
+apt-get -y install python-httplib2
 apt-get -y install couchbase-server
 
 echo "Calling util.sh..."
