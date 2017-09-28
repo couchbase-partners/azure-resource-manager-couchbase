dc9354122796b0d6074b0f34b5ac70fe277845b3
benofben <ben.lackey@outlook.com>
2017-09-28 16:24:25 -0700

    retry loop for metadata service

--- extensions/server.sh
+++ extensions/server.sh
@@ -34,10 +34,15 @@ adjustTCPKeepalive
 
 echo "Configuring Couchbase Server..."
 apt-get -y install jq
-nodeIndex=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2017-04-02" \
-  | jq ".name" \
-  | sed 's/.*_//' \
-  | sed 's/"//'`
+
+nodeIndex="null"
+while [[ $nodeIndex == "null" ]]
+do
+  nodeIndex=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2017-04-02" \
+    | jq ".name" \
+    | sed 's/.*_//' \
+    | sed 's/"//'`
+done
 
 nodeDNS='vm'$nodeIndex'.server-'$uniqueString'.'$location'.cloudapp.azure.com'
 rallyDNS='vm0.server-'$uniqueString'.'$location'.cloudapp.azure.com'
