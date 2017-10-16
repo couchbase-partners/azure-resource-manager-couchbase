fe93212ce72a4e6649628b4d30a50f345f3cfdda
benofben <ben.lackey@outlook.com>
2017-10-16 13:34:16 -0700

    more notes on metadata

--- extensions/server.sh
+++ extensions/server.sh
@@ -37,6 +37,7 @@ echo "Configuring Couchbase Server..."
 
 # We can get the index directly with this, but unsure how to test for sucess.  Come back to later...
 #nodeIndex = `curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-04-02&format=text"`
+# good example here https://github.com/bonggeek/Samples/blob/master/imds/imds.sh
 
 nodeIndex="null"
 while [[ $nodeIndex == "null" ]]
