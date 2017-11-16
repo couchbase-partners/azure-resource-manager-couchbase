247e606644cc920146e547424052980a1a745959
benofben <ben.lackey@outlook.com>
2017-11-15 16:23:04 -0800

    vnet and nsg done

--- generator/deployment.py
+++ generator/deployment.py
@@ -12,12 +12,15 @@ def main():
     print('Parameters: ' + str(parameters))
 
     template={
-      "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-      "contentVersion": "1.0.0.0",
-      "parameters": {},
-      "variables": {},
-      "resources": [],
-      "outputs": {}
+        "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+        "contentVersion": "1.0.0.0",
+        "parameters": {},
+        "variables": {
+            "extensionUrl": "https://raw.githubusercontent.com/couchbase-partners/azure-resource-manager-couchbase/master/extensions/",
+            "uniqueString": "[uniquestring(resourceGroup().id, deployment().name)]"
+        },
+        "resources": [],
+        "outputs": generateOutputs()
     }
 
     license = parameters['license']
@@ -35,9 +38,179 @@ def generateCluster(cluster):
     resources = []
     clusterName = cluster['cluster']
     region = cluster['region']
+
+    resources+=generateNetworkSecurityGroups()
+    resources+=generateVirtualNetwork()
     for group in cluster['groups']:
         resources+=generateGroup(group)
     return resources
+
+def generateNetworkSecurityGroups():
+    networkSecurityGroups={
+        "apiVersion": "2016-06-01",
+        "type": "Microsoft.Network/networkSecurityGroups",
+        "name": "networksecuritygroups",
+        "location": "[parameters('location')]",
+        "properties": {
+            "securityRules": [
+                {
+                    "name": "SSH",
+                    "properties": {
+                        "description": "SSH",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "22",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 100,
+                        "direction": "Inbound"
+                    }
+                },
+                {
+                    "name": "ErlangPortMapper",
+                    "properties": {
+                        "description": "Erlang Port Mapper ( epmd )",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "4369",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 101,
+                        "direction": "Inbound"
+                    }
+                },
+                {
+                    "name": "SyncGateway",
+                    "properties": {
+                        "description": "Sync Gateway",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "4984-4985",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 102,
+                        "direction": "Inbound"
+                    }
+                },
+                {
+                    "name": "Server",
+                    "properties": {
+                        "description": "Server",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "8091-8094",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 103,
+                        "direction": "Inbound"
+                    }
+                },
+                {
+                    "name": "Index",
+                    "properties": {
+                        "description": "Index",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "9100-9105",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 104,
+                        "direction": "Inbound"
+                    }
+                },
+                {
+                    "name": "Internal",
+                    "properties": {
+                        "description": "Internal",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "9998-9999",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 105,
+                        "direction": "Inbound"
+                    }
+                },
+                {
+                    "name": "XDCR",
+                    "properties": {
+                        "description": "XDCR",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "11207-11215",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 106,
+                        "direction": "Inbound"
+                    }
+                },
+                {
+                    "name": "SSL",
+                    "properties": {
+                        "description": "SSL",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "18091-18093",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 107,
+                        "direction": "Inbound"
+                    }
+                },
+                {
+                    "name": "NodeDataExchange",
+                    "properties": {
+                        "description": "Node data exchange",
+                        "protocol": "Tcp",
+                        "sourcePortRange": "*",
+                        "destinationPortRange": "21100-21299",
+                        "sourceAddressPrefix": "Internet",
+                        "destinationAddressPrefix": "*",
+                        "access": "Allow",
+                        "priority": 108,
+                        "direction": "Inbound"
+                    }
+                }
+            ]
+        }
+    }
+    return networkSecurityGroups
+
+def generateVirtualNetwork():
+    virtualNetwork={
+        "name": "vnet",
+        "type": "Microsoft.Network/virtualNetworks",
+        "apiVersion": "2015-06-15",
+        "location": "[parameters('location')]",
+        "dependsOn": [
+            "Microsoft.Network/networkSecurityGroups/networksecuritygroups"
+        ],
+        "properties": {
+            "addressSpace": {
+                "addressPrefixes": ["10.0.0.0/8"]
+            },
+            "subnets": [
+                {
+                    "name": "subnet",
+                    "properties": {
+                        "addressPrefix": "10.0.0.0/16",
+                        "networkSecurityGroup": {
+                            "id": "[resourceId('Microsoft.Network/networkSecurityGroups', 'networksecuritygroups')]"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+    return virtualNetwork
 
 def generateGroup(group):
     groupName = group['group']
@@ -48,5 +221,18 @@ def generateGroup(group):
 
     resources={}
     return resources
+
+def generateOutputs():
+    outputs={
+        "serverAdminURL": {
+            "type": "string",
+            "value": "[concat('http://vm0.server-', variables('uniqueString'), '.', parameters('location'), '.cloudapp.azure.com:8091')]"
+        },
+        "syncGatewayAdminURL": {
+            "type": "string",
+            "value": "[concat('http://vm0.syncgateway-', variables('uniqueString'), '.', parameters('location'), '.cloudapp.azure.com:4985/_admin/')]"
+        }
+    }
+    return outputs
 
 main()
