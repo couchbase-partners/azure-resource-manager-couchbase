03fb50bd2c271cf5f01b1067156d2becb0a4e861
Rafael Godinho <rgodinho@microsoft.com>
2018-04-19 17:27:34 -0700

    Simple scenario

---
+++ generator/debug.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#Install Python
+#sudo apt install python-minimal
+
+#Install pip
+#sudo apt install python-pip
+
+#Install pyyaml
+#sudo pip install pyyaml
+
+./deploy.sh simple rgName$(date +%Y%m%d)
