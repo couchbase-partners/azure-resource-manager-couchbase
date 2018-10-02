5cf68d627953756dd780da151917400403ff39ef
Shomari Liburd <shomari.liburd@couchbase.com>
2018-10-02 07:50:36 -0700

    Work for granular.yaml only.Adding functionality for custom vnets and subnets.Multiple clusters possibly coming later.sgw or syncgateway as a service launches syncgateway and ignores other services.Not functional yet

--- generator/deploy.sh
+++ generator/deploy.sh
@@ -6,5 +6,5 @@ RESOURCE_GROUP=$2
 # create generatedTemplate.json
 python deployment.py parameters/${PARAMETERS_FILE}.yaml
 
-az group create --name $RESOURCE_GROUP --location westus --output table
-az group deployment create --verbose --template-file generatedTemplate.json --resource-group $RESOURCE_GROUP --output table
+ az group create --name $RESOURCE_GROUP --location westus --output table
+ az group deployment create --verbose --template-file generatedTemplate.json --parameters @parameters/parameters.json --resource-group $RESOURCE_GROUP --output table
