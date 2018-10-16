644e72fe7dcd717dfd1638f6b47c4badb9817591
Shomari Liburd <shomari.liburd@couchbase.com>
2018-10-16 08:56:39 -0700

    add incremental to the deploy scripts though it should be the default. There seems to be some inconsistent behavior with how deployments work after multiple deployments. Closed NSG to virtualNetwork in most cases and Internet for XDCR and SGW granular_rally can create a subnet but it is still clusters->clustername-subnet.  You can add a subnetAddrPrefix to have teh subnet created The subnet creation feature is still in progress so remove subnetAddrPrefix and create your own if you are having issues. changed the password in parameters to remove ! For now in parameters uniqueString and prefix is unused

--- generator/deploy_rally.sh
+++ generator/deploy_rally.sh
@@ -7,6 +7,6 @@ REGION=${3-$default}
 
 #az group deployment create --template-file uniqueString.json --resource-group $RESOURCE_GROUP --output table
 
-python deployment_ms.py parameters/${PARAMETERS_FILE}.yaml
+python deployment_ms.py parameters/granular_rally.yaml
 #az group create --name $RESOURCE_GROUP --location $REGION --output table
-az group deployment create -n NonRallyDeployment --mode Incremental --verbose --template-file generatedTemplate.json --parameters @parameters/parameters.json --resource-group $RESOURCE_GROUP --output table
+az group deployment create -n RallyDeployment --mode Incremental --verbose --template-file generatedTemplate.json --parameters @parameters/parameters.json --resource-group $RESOURCE_GROUP --output table
