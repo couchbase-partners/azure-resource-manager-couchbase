0618a79a80d2c351ed7e23d17fe29b6ef89b78a4
Shomari Liburd <shomari.liburd@couchbase.com>
2019-05-15 21:18:29 -0700

    deleting beta artifacts from 6.0.0-beta updating api versions and values for mainTemplate*

--- simple/deploy_beta.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-RESOURCE_GROUP=$1
-
-az group create --name $RESOURCE_GROUP --location westus --output table
-az group deployment create --verbose --template-file mainTemplate_beta.json --parameters @mainTemplateParameters_beta.json --resource-group $RESOURCE_GROUP --output table
