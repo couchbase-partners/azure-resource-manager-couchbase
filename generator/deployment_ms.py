097884d411ba7e9771f20e49ac71a66129d4ef0b
Shomari Liburd <shomari.liburd@couchbase.com>
2019-05-22 03:12:49 -0700

    Adding new regions that are AZ compatiblity No spaces are allowed in Couchbase server Group names (scripting restriction)

--- generator/deployment_ms.py
+++ generator/deployment_ms.py
@@ -81,7 +81,14 @@ def generateParameters(clusters):
             "allowedValues": [
                 "centralus",
                 "westus2",
-                "eastus2"
+                "eastus",
+                "eastus2",
+                "francecentral",
+                "japaneast",
+                "southeastasia",
+                "westeurope",
+                "uksouth",
+                "northeurope"
             ] 
         }
     }
