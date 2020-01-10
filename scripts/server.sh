37cff8291656a9a9f1f0b4313228810a08717a98
Shomari Liburd <shomari.liburd@couchbase.com>
2020-01-09 16:46:40 -0800

    changing --user -ppas shortcut to --username and --password respectively

--- scripts/server.sh
+++ scripts/server.sh
@@ -88,8 +88,8 @@ echo "Running couchbase-cli node-init"
   --node-init-data-path=/datadisk/data \
   --node-init-index-path=/datadisk/index \
   --node-init-analytics-path=/datadisk/analytics \
-  --user=$adminUsername \
-  --pass=$adminPassword
+  --username=$adminUsername \
+  --password=$adminPassword
 
 if [[ $nodeIndex == "0" ]]
 then
@@ -115,8 +115,8 @@ else
   do
     output=`./couchbase-cli server-add \
       --cluster=$rallyDNS \
-      --user=$adminUsername \
-      --pass=$adminPassword \
+      --username=$adminUsername \
+      --password=$adminPassword \
       --server-add=$nodeDNS \
       --server-add-username=$adminUsername \
       --server-add-password=$adminPassword \
@@ -131,8 +131,8 @@ else
   do
     output=`./couchbase-cli rebalance \
       --cluster=$rallyDNS \
-      --user=$adminUsername \
-      --pass=$adminPassword`
+      --username=$adminUsername \
+      --password=$adminPassword`
     echo rebalance output \'$output\'
     sleep 10
   done
